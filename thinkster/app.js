d3e56cffb9181d7e426a33f3ba79fa12ca1651ff
RELIGARE\riju <mailmeonriju@gmail.com>
2016-09-28 18:28:45 +0530

    foo

--- thinkster/app.js
+++ thinkster/app.js
@@ -1,12 +1,18 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
 app.factory('posts', [function () {
-        // service body
+        var o = {
+            posts: []
+        };
+        return o;
     }]);
 
 app.controller('MainCtrl', [
-    '$scope',
-    function ($scope) {
+    '$scope', 'posts',
+    function ($scope, posts) {
+
+        $scope.posts = posts.posts;
+
         $scope.test = 'Hello world!';
         $scope.posts = [
             {title: 'post 1', upvotes: 5},
@@ -23,10 +29,26 @@ app.controller('MainCtrl', [
             $scope.posts.push({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0
+                upvotes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
             });
             $scope.title = '';
             $scope.link = '';
+        };
+
+        $scope.addComment = function () {
+            if ($scope.body === '') {
+                return;
+            }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
         };
 
         $scope.incrementUpvotes = function (post) {
@@ -35,3 +57,32 @@ app.controller('MainCtrl', [
 
     }]);
 
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function ($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+    }]);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                })
+
+                .state('posts', {
+                    url: '/posts/{id}',
+                    templateUrl: '/posts.html',
+                    controller: 'PostsCtrl'
+                });
+
+
+        $urlRouterProvider.otherwise('home');
+    }]);
